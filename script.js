11f96fbda344aba9ed6920babe7a3b7680181845
atulsharma3982 <atulsharma8544@gmail.com>
2025-10-12 21:11:41 +0530

    used functions and better data storage

--- script.js
+++ script.js
@@ -6,18 +6,45 @@ let completedTasks = JSON.parse(localStorage.getItem("completedTasks")) || [];
 
 let count = localStorage.getItem("count") || 0;
 
-for (const element of tasksToDo) {
+if(tasksToDo.length === 0 && completedTasks.length === 0){
+    count = 0;
+}
+
+function createTask(id, data, checked = false) {
     let li = document.createElement("li");
-    li.id = element;
-    li.innerHTML = JSON.parse(localStorage.getItem(element));
+    let checkbox = document.createElement("input");
+    let p = document.createElement("p");
+    let del = document.createElement("button");
+    let img = document.createElement("img");
+    let div = document.createElement("div");
+    del.appendChild(img);
+    del.className = "del";
+    img.src = "assets/delete-2-svgrepo-com.svg";
+    img.alt = "del";
+    img.style.width = "15px";
+    checkbox.type = "checkbox";
+    checkbox.name = "task";
+    // checkbox.checked = checked;
+    li.id = id;
+    p.innerHTML = data;
+    div.append(checkbox);
+    div.append(p);
+    li.appendChild(div);
+    li.appendChild(del);
+    return li;
+}
+
+
+
+for (const element of tasksToDo) {
+    let task = JSON.parse(localStorage.getItem(element));
+    let li = createTask(task.id,task.data);
     li.querySelector("input").checked = false;
     leftul.appendChild(li);
 }
 for (const element of completedTasks) {
-    let li = document.createElement("li");
-    li.id = element;
-    li.innerHTML = JSON.parse(localStorage.getItem(element));
-    // let task = e.target.closest("li");
+    let task = JSON.parse(localStorage.getItem(element));
+    let li = createTask(task.id,task.data);
     let p = li.querySelector("p");
     p.innerHTML = `<s>${p.textContent}</s>`
     li.querySelector("input").checked = true;
@@ -53,7 +80,7 @@ btn.addEventListener("click", () => {
     body.append(newTaskWindow);
     input.focus();
     btn.style.display = "none";
-    cancel.addEventListener("click", () => {
+    cancel.addEventListener("click", cancelTask = () => {
         btn.removeAttribute('style');
         overlay.remove();
         newTaskWindow.remove();
@@ -61,34 +88,23 @@ btn.addEventListener("click", () => {
     })
     done.addEventListener("click", () => {
         if (input.value.trim().length) {
-            let li = document.createElement("li");
-            let checkbox = document.createElement("input");
-            let p = document.createElement("p");
-            let del = document.createElement("button");
-            let img = document.createElement("img");
-            let div = document.createElement("div");
-            del.appendChild(img);
-            del.className="del";
-            img.src = "assets/delete-2-svgrepo-com.svg";
-            img.alt = "del";
-            img.style.width = "15px";
-            checkbox.type = "checkbox";
-            checkbox.name = "task";
-            li.id = `task${count++}`;
+            console.log("working")
+            let task = {};
+            let li = createTask(`task${count++}`, input.value.trim());
             localStorage.setItem("count", count);
             tasksToDo.push(li.id);
+            task = {
+                id : li.id,
+                data : input.value.trim(),
+                // checked : false
+            }
             localStorage.setItem("tasksToDo", JSON.stringify(tasksToDo));
-            p.innerHTML = input.value.trim();
-            div.append(checkbox);
-            div.append(p);
-            li.appendChild(div);
-            li.appendChild(del);
-            localStorage.setItem(li.id, JSON.stringify(li.innerHTML));
+            localStorage.setItem(li.id, JSON.stringify(task));
             ul.appendChild(li);
             btn.removeAttribute('style');
             overlay.remove();
             newTaskWindow.remove();
-            cancel.removeEventListener("click");
+            cancel.removeEventListener("click" , cancelTask());
         }
         else {
             alert("Empty task can't be added");
@@ -109,9 +125,9 @@ document.body.addEventListener("keydown", (e) => {
 
 if (leftul) {
     leftul.addEventListener("click", (e) => {
+        let task = e.target.closest("li");
         if (e.target.type === "checkbox") {
             if (e.target.checked) {
-                let task = e.target.closest("li");
                 let p = task.querySelector("p");
                 p.innerHTML = `<s>${p.textContent}</s>`
                 task.querySelector("input").checked = true;
@@ -121,55 +137,45 @@ if (leftul) {
                 if (index !== -1) {
                     tasksToDo.splice(index, 1);
                 }
-                localStorage.setItem("tasksToDo", JSON.stringify(tasksToDo));
                 rightul.appendChild(task);
             }
         }
+        if (e.target.closest(".del")) {
+            let index = tasksToDo.findIndex(t => t === task.id);
+            if (index !== -1) {
+                tasksToDo.splice(index, 1);
+            }
+            localStorage.removeItem(task.id);
+            task.remove();
+        }
+        localStorage.setItem("tasksToDo", JSON.stringify(tasksToDo));
     })
 }
 if (rightul) {
     rightul.addEventListener("click", (e) => {
+        let task = e.target.closest("li");
         if (e.target.type === "checkbox") {
             if (!e.target.checked) {
-                let task = e.target.closest("li");
                 let p = task.querySelector("p");
                 p.innerHTML = p.textContent
                 task.querySelector("input").checked = false;
                 tasksToDo.push(task.id);
                 localStorage.setItem("tasksToDo", JSON.stringify(tasksToDo));
-                // tasksToDo.slice();
                 let index = completedTasks.findIndex(t => t === task.id);
                 if (index !== -1) {
                     completedTasks.splice(index, 1);
                 }
-                localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
                 leftul.appendChild(task);
             }
         }
-    })
-}
-
-leftul.addEventListener("click", (e) => {
-    if (e.target.closest(".del")) {
-        let task = e.target.closest("li");
-        let index = tasksToDo.findIndex(t => t === task.id);
-        if (index !== -1) {
-            tasksToDo.splice(index, 1);
-        }
-        localStorage.setItem("tasksToDo", JSON.stringify(tasksToDo));
-        localStorage.removeItem(task.id);
-        task.remove();
-    }
-})
-rightul.addEventListener("click", (e) => {
-    if (e.target.closest(".del")) {
-        let task = e.target.closest("li");
-        let index = completedTasks.findIndex(t => t === task.id);
-        if (index !== -1) {
-            completedTasks.splice(index, 1);
+        if (e.target.closest(".del")) {
+            let index = completedTasks.findIndex(t => t === task.id);
+            if (index !== -1) {
+                completedTasks.splice(index, 1);
+            }
+            localStorage.removeItem(task.id);
+            task.remove();
         }
         localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
-        localStorage.removeItem(task.id);
-        task.remove();
-    }
-})
+    })
+}
